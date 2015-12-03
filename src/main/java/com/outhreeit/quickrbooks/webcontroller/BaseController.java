0cb3c1482d69350018898e35bab02e9757300163
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-03 14:37:50 +0000

    created baseController

---
+++ src/main/java/com/outhreeit/quickrbooks/webcontroller/BaseController.java
@@ -0,0 +1,47 @@
+package com.outhreeit.quickrbooks.webcontroller;
+
+import com.outhreeit.quickrbooks.entities.BaseEntity;
+import com.outhreeit.quickrbooks.services.BaseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by g on 12/3/15.
+ */
+@RestController
+public abstract class BaseController<T extends BaseEntity> implements IBaseController<T> {
+
+    protected BaseService<T> service;
+
+    public BaseController(BaseService service) {
+        this.service = service;
+    }
+
+
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public List<T> getAll() {
+        System.out.println("hello");
+        return service.getAll();
+    }
+
+    @RequestMapping(value="", method=RequestMethod.POST)
+    public void add(@RequestBody T t) {
+        service.add(t);
+    }
+
+    @RequestMapping(value="", method=RequestMethod.PUT)
+    public void update(@RequestBody T t) {
+        service.update(t);
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.POST)
+    public T getByID(@PathVariable Integer id) {
+        return (T) service.getByID(id);
+    }
+}
