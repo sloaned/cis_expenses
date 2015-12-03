fd31f2719bec8b62de8ba7a9fa0b050f654e7be0
Dan Sloane <dsloane@catalystdevworks.com>
2015-12-03 15:13:56 -0800

    first commit

--- src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
+++ src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
@@ -1,16 +1,43 @@
 package com.outhreeit.quickrbooks;
 
-import org.springframework.context.annotation.Configuration;
+import javax.naming.directory.DirContext;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.support.LdapUtils;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Configuration
-public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
+public class SpringSecurityConfigurer{
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("root").authorities("USER");
-    }
+	@Autowired
+	private DataSource dataSource; 
+	
+	@Autowired
+	private ContextSource contextSource;
+	
+	@Autowired
+	public void configureGlobal(
+				AuthenticationManagerBuilder auth)throws Exception{
+		auth
+				.ldapAuthentication()
+					.userDnPatterns("uid={0},ou=people")
+					.groupSearchBase("ou=groups");
+	}
+	
+	public boolean authenticate(String userDn, String credentials){
+		DirContext ctx = null;
+		try{
+			ctx = contextSource.getContext(userDn, credentials);
+			return true;
+		} catch(Exception e){
+			return false;
+		} finally{
+			LdapUtils.closeContext(ctx);
+		}
+	}
+	
+    
 }
