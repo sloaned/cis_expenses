b8165f973aa1d1e9bba4abddf5d44e623f5395d5
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-03 14:24:33 +0000

    created baseDao

--- src/main/java/com/outhreeit/quickrbooks/daos/IBaseDao.java
+++ src/main/java/com/outhreeit/quickrbooks/daos/IBaseDao.java
@@ -2,6 +2,12 @@ package com.outhreeit.quickrbooks.daos;
 
 import com.outhreeit.quickrbooks.entities.BaseEntity;
 
-public interface IBaseDao<T extends BaseEntity> {
+import java.util.List;
 
+public interface IBaseDao<T extends BaseEntity> {
+     void add(T t);
+    void update(T t);
+    void delete(Integer id);
+    List<T> getAll();
+    T getByID(Integer id);
 }
