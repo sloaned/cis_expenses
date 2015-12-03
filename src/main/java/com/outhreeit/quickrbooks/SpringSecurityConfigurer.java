e02d0a6f5058cca6c6f2fc680d4a0b68d6d43ff1
Dan Sloane <dsloane@catalystdevworks.com>
2015-12-03 15:40:42 -0800

    added dependencies

--- src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
+++ src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
@@ -4,14 +4,30 @@ import javax.naming.directory.DirContext;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.support.LdapUtils;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
+@Configuration
 @EnableWebSecurity
-public class SpringSecurityConfigurer{
+public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
 
+	@Bean
+	public ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider(){
+		ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("catalystdevworks.com", "ldap://ad.catalystdevworks.com:389");
+		provider.setConvertSubErrorCodesToExceptions(true);
+		provider.setUseAuthenticationRequestCredentials(true);
+		//provider.setAuthoritiesMapper(myAuthoritiesMapper());
+		
+		provider.setUseAuthenticationRequestCredentials(true);
+		return provider;
+	}
+	
 	@Autowired
 	private DataSource dataSource; 
 	
