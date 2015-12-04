231e09c33ad0dd7d248f674ba850096f6194616d
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-04 10:21:18 +0000

    forgot annotation

--- src/main/java/com/outhreeit/quickrbooks/entities/User.java
+++ src/main/java/com/outhreeit/quickrbooks/entities/User.java
@@ -1,7 +1,9 @@
 package com.outhreeit.quickrbooks.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Entity
 @Table( name = "usr")
 public class User extends BaseEntity{
 }
