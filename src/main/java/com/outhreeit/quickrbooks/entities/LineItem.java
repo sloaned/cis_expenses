a5ea4f27573113ea8be285c5a42a3c078c66b662
Greg Fisher <gfisher@catalystitservices.com>
2015-12-04 11:43:22 -0800

    tables are created properly

--- src/main/java/com/outhreeit/quickrbooks/entities/LineItem.java
+++ src/main/java/com/outhreeit/quickrbooks/entities/LineItem.java
@@ -1,13 +1,19 @@
 package com.outhreeit.quickrbooks.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Currency;
 
 /**
  * Created by gfisher on 12/3/2015.
  */
 @Entity
-public class LineItem extends BaseEntity {
+public class LineItem{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     Currency datMoney;
+    @OneToOne
     LineItemType type;
 }
