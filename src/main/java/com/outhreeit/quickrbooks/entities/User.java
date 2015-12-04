ad816852cabf15e586bc015e1d1b5232112a0d9c
Greg Fisher <gfisher@catalystitservices.com>
2015-12-04 12:23:59 -0800

    modified user

--- src/main/java/com/outhreeit/quickrbooks/entities/User.java
+++ src/main/java/com/outhreeit/quickrbooks/entities/User.java
@@ -1,10 +1,12 @@
 package com.outhreeit.quickrbooks.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * Created by gfisher on 12/4/2015.
  */
 @Entity
+@Table(name="usr")
 public class User extends BaseEntity {
 }
