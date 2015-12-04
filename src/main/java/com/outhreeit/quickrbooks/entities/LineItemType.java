d7b0bffe477a85079c8b925d5a9c56611bc8cf73
Greg Fisher <gfisher@catalystitservices.com>
2015-12-04 10:40:09 -0800

    I'm breaking stuff

--- src/main/java/com/outhreeit/quickrbooks/entities/LineItemType.java
+++ src/main/java/com/outhreeit/quickrbooks/entities/LineItemType.java
@@ -1,8 +1,11 @@
 package com.outhreeit.quickrbooks.entities;
 
+import javax.persistence.Entity;
+
 /**
  * Created by gfisher on 12/3/2015.
  */
+@Entity
 public class LineItemType extends BaseEntity {
 
 }
---
+++ src/main/java/com/outhreeit/quickrbooks/entities/Usr.java
@@ -0,0 +1,7 @@
+package com.outhreeit.quickrbooks.entities;
+
+/**
+ * Created by gfisher on 12/4/2015.
+ */
+public class Usr {
+}
