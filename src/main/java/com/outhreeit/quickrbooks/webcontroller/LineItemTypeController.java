7258362c2965067b9274c66fc6cb64a6016ad526
Greg Fisher <gfisher@catalystitservices.com>
2015-12-07 09:14:33 -0800

    Overrode methods so users can't add, update, or delete line items via postman

--- src/main/java/com/outhreeit/quickrbooks/webcontroller/LineItemTypeController.java
+++ src/main/java/com/outhreeit/quickrbooks/webcontroller/LineItemTypeController.java
@@ -4,8 +4,7 @@ import com.outhreeit.quickrbooks.entities.LineItemType;
 import com.outhreeit.quickrbooks.services.BaseService;
 import com.outhreeit.quickrbooks.services.LineItemTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by gfisher on 12/4/2015.
@@ -17,5 +16,20 @@ public class LineItemTypeController extends BaseController<LineItemType>{
     @Autowired
     public LineItemTypeController(LineItemTypeService service) {
         super(service);
+    }
+
+    @Override
+    public void add(@RequestBody LineItemType t) {
+        System.out.println("I can't let you do that");
+    }
+
+    @Override
+    public void update(@RequestBody LineItemType t) {
+        System.out.println("I can't let you do that");
+    }
+
+    @Override
+    public void delete(@PathVariable Integer id) {
+        System.out.println("I can't let you do that");
     }
 }
