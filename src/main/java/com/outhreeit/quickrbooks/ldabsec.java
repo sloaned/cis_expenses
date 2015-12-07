390456c4d884033d0de1f65ea4261036df5d09ec
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-07 09:06:01 +0000

    pom changes

---
+++ src/main/java/com/outhreeit/quickrbooks/ldabsec.java
@@ -0,0 +1,8 @@
+package com.outhreeit.quickrbooks;
+
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.support.LdapContextSource;
+
+public class ldabsec  extends AbstractLdapAuthenticationProvider {
+
+}
