71473cdbe69f14c8c2afdda75247aaae8a805f54
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-08 11:28:21 +0000

    reverted projectDao

--- src/main/java/com/outhreeit/quickrbooks/daos/ProjectDao.java
+++ src/main/java/com/outhreeit/quickrbooks/daos/ProjectDao.java
@@ -1,13 +1,9 @@
 package com.outhreeit.quickrbooks.daos;
 
 import com.outhreeit.quickrbooks.daos.Interface.IProjectDao;
-import com.outhreeit.quickrbooks.entities.ExpenseReport;
 import com.outhreeit.quickrbooks.entities.Project;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Repository
 @Transactional
@@ -16,12 +12,4 @@ public class ProjectDao extends BaseDao<Project> implements IProjectDao {
         super(Project.class);
     }
 
-    public Project getByID(Integer id){
-        Set<ExpenseReport> reports=  new HashSet<>(em.createQuery("Select e from ExpenseReport e join e.project p " +
-            "where p.id = :id",ExpenseReport.class).setParameter("id",id).getResultList());
-        Project p = super.getByID(id);
-        p.setReports(reports);
-        System.out.println(p);
-        return p;
-    }
 }
