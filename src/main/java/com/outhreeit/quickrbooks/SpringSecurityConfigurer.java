9ac4924cf8534dc2910f01a69f2f395b9860810b
Daniel Sloane <Daniel Sloane>
2015-12-07 19:58:55 -0800

    changes

--- src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
+++ src/main/java/com/outhreeit/quickrbooks/SpringSecurityConfigurer.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.support.LdapUtils;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
@@ -17,42 +18,33 @@ import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAu
 @EnableWebSecurity
 public class SpringSecurityConfigurer extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		http
+				.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.and()
+				.logout()
+				.and()
+				.rememberMe();
+	}
+	
 	@Bean
 	public ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider(){
-		ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("catalystdevworks.com", "ldap://ad.catalystdevworks.com:389");
+		ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("catalystsolves.com", "ldap://catalystsolves.com:389");
 		provider.setConvertSubErrorCodesToExceptions(true);
 		provider.setUseAuthenticationRequestCredentials(true);
-		//provider.setAuthoritiesMapper(myAuthoritiesMapper());
 		
-		provider.setUseAuthenticationRequestCredentials(true);
 		return provider;
-	}
-	
-	@Autowired
-	private DataSource dataSource; 
-	
-	@Autowired
-	private ContextSource contextSource;
-	
-	@Autowired
-	public void configureGlobal(
-				AuthenticationManagerBuilder auth)throws Exception{
-		auth
-				.ldapAuthentication()
-					.userDnPatterns("uid={0},ou=people")
-					.groupSearchBase("ou=groups");
-	}
-	
-	public boolean authenticate(String userDn, String credentials){
-		DirContext ctx = null;
-		try{
-			ctx = contextSource.getContext(userDn, credentials);
-			return true;
-		} catch(Exception e){
-			return false;
-		} finally{
-			LdapUtils.closeContext(ctx);
-		}
 	}
 	
     
