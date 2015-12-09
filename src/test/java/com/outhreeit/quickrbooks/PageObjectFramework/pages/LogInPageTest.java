8b2743f1fcda7d0945726e6f209069805b221aaa
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-09 11:44:43 -0800

    wrote selenium test for the log in the page

---
+++ src/test/java/com/outhreeit/quickrbooks/PageObjectFramework/pages/LogInPageTest.java
@@ -0,0 +1,57 @@
+package com.outhreeit.quickrbooks.PageObjectFramework.pages;
+
+import com.outhreeit.quickrbooks.SeleniumFramework.Pages.LogInPage;
+import com.outhreeit.quickrbooks.SeleniumFramework.TestPageObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ddelaney on 12/9/2015.
+ */
+public class LogInPageTest extends TestPageObject {
+    private final String VALID_USERNAME = "user";
+    private final String SUPER_SECURE_VALID_PASSWORD = "root";
+
+    @Test
+    public void typeInAValidUserNameAndPasswordAndClickSubmit(){
+        LogInPage logInPage = new LogInPage(driver);
+        String expectedResult = "QuickrBooks";
+
+        logInPage.sendKeys(By.name("username"), VALID_USERNAME);
+        logInPage.sendKeys(By.name("password"), SUPER_SECURE_VALID_PASSWORD);
+        logInPage.click(By.name("submit"));
+
+        String actualResult = logInPage.getTitle();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void typeInAInvalidUserNameAndPasswordAndClickSubmit(){
+        LogInPage logInPage = new LogInPage(driver);
+        String expectedResult = "Login Page";
+
+        logInPage.sendKeys(By.name("username"), SUPER_SECURE_VALID_PASSWORD);
+        logInPage.sendKeys(By.name("password"), VALID_USERNAME);
+        logInPage.click(By.name("submit"));
+
+        String actualResult = logInPage.getTitle();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void tryToNavagateToAPageWhenYouHavNotLoggedInYet(){
+        LogInPage logInPage = new LogInPage(driver);
+        String expectedResult = "Login Page";
+
+        logInPage.goTo("localhost:8080/expense-report");
+
+        String actualResult = logInPage.getTitle();
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+
+}
