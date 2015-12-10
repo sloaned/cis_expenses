44777003cc21d958ec7b318c42df27afbae5a174
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-10 10:23:42 -0800

    wrote selenium test for the expense report page

--- src/main/java/com/outhreeit/quickrbooks/SeleniumFramework/Pages/HomePage.java
+++ src/main/java/com/outhreeit/quickrbooks/SeleniumFramework/Pages/HomePage.java
@@ -3,14 +3,16 @@ package com.outhreeit.quickrbooks.SeleniumFramework.Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Scanner;
+
 /**
  * Created by gfisher on 12/9/2015.
  */
 public class HomePage extends LogInPage {
     public HomePage(WebDriver driver) {
         super(driver);
-        sendKeys(By.name("username"), "user");
-        sendKeys(By.name("password"), "root");
+        sendKeys(By.name("username"), "LDAPUser");
+        sendKeys(By.name("password"), "@oxXiS,;AIry\\{8wixuf3;f^?");
         click(By.name("submit"));
     }
 
